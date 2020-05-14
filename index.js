04801ab470642109b257ee6562c5d4f28e74c68f
Calvin Metcalf <cmetcalf@appgeo.com>
2020-05-14 08:16:35 -0400

    awkward bug

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ module.exports = (array, num, opts={}) => {
   switch (algo) {
     case 'spaceFillingCurve':
     case 'sfc':
-      return spaceFillingCurve(array.slice(), 10, getCoord)
+      return spaceFillingCurve(array.slice(), num, getCoord)
     case 'kmeans':
     case 'k-means':
-      return kmeans(array, 10, getCoord)
+      return kmeans(array, num, getCoord)
     case 'rtree':
-      return rtree(array, 10, getCoord)
+      return rtree(array, num, getCoord)
     case 'rtree-plus':
     case 'rtree+':
-      return rtree(array, 10, getCoord, true)
+      return rtree(array, num, getCoord, true)
     default: throw new Error(`unknown algorhthem: ${algo}`)
   }
 }
